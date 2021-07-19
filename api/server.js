58377658c498314d732f3280c6dff8917fd11be9
Civoremo <civoremon@gmail.com>
2021-07-19 18:26:14 -0400

    user route

--- api/server.js
+++ api/server.js
@@ -4,11 +4,15 @@ require("dotenv").config();
 
 const { cors, helmet, morgan, express } = require("./configMW");
 
+const usersRoutes = require("../knex/routes/usersRouter");
+
 const server = express();
 server.use(helmet());
 server.use(morgan("short"));
 server.use(express.json());
 server.use(cors());
+
+server.use("/users", usersRoutes);
 
 server.get("/", (req, res) => {
   res.send("Server is connected.");
