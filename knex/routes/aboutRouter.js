94e17ef9acba1252474b89ffc28b4f194d113657
Civoremo <civoremon@gmail.com>
2021-07-20 18:33:46 -0400

    about section router post, put

---
+++ knex/routes/aboutRouter.js
@@ -0,0 +1,37 @@
+/** @format */
+
+const { express } = require("../../api/configMW");
+const { protected } = require("../middleware/protectedMW");
+
+const aboutDB = require("../helpers/aboutDB");
+const router = express.router();
+
+router.post("/section/register", protected, (req, res) => {
+  const info = req.body;
+
+  aboutDB
+    .registerAbout(info)
+    .then(result => {
+      console.log("about section create", result);
+      res.status(201).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "About section creation failed." });
+    });
+});
+
+router.put("/section/update", protected, (req, res) => {
+  const info = req.body;
+
+  aboutDB
+    .updateAbout(info)
+    .then(result => {
+      console.log("about section update", result);
+      res.status(200).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Aboust section update failed." });
+    });
+});
+
+module.exports = router;
