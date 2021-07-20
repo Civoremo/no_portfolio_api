f97b1002549e86e9725914935e747fb7f0238e7a
Civoremo <civoremon@gmail.com>
2021-07-20 17:57:35 -0400

    table image req tweaks

--- knex/migrations/20210720211928_04_projectsTable.js
+++ knex/migrations/20210720211928_04_projectsTable.js
@@ -4,7 +4,12 @@ exports.up = function (knex) {
   return knex.schema.createTable("projects", tbl => {
     tbl.increments("id").unsigned();
     tbl.string("title").notNullable();
-    tbl.text("gifImage").notNullable();
+    tbl
+      .text("gifImage")
+      .notNullable()
+      .default(
+        "https://res.cloudinary.com/civoremo/image/upload/v1626818071/web_portfolio/portfolio_default.png"
+      );
     tbl.text("description").notNullable();
     tbl.text("stack");
     tbl.text("liveLink");
