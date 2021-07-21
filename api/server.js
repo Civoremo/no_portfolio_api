3a009c754dec390054e4e965f5dc02273427bff2
Civoremo <civoremon@gmail.com>
2021-07-20 20:59:01 -0400

    contact info routes

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const { cors, helmet, morgan, express } = require("./configMW");
 const usersRoutes = require("../knex/routes/usersRouter");
 const aboutRouter = require("../knex/routes/aboutRouter");
 const projectRouter = require("../knex/routes/projectRouter");
+const contactRouter = require("../knex/routes/contactRouter");
 
 const server = express();
 server.use(helmet());
@@ -17,6 +18,7 @@ server.use(cors());
 server.use("/users", usersRoutes);
 server.use("/about", aboutRouter);
 server.use("/projects", projectRouter);
+server.use("/contact", contactRouter);
 
 server.get("/", (req, res) => {
   res.send("Server is connected.");
---
+++ knex/routes/contactRouter.js
@@ -0,0 +1,93 @@
+/** @format */
+
+const { express } = require("../../api/configMW");
+const { protected } = require("../middleware/protectedMW");
+
+const contactDB = require("../helpers/contactDB");
+const router = express.router();
+
+router.post("/register", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .registerContact(info)
+    .then(result => {
+      console.log("contact registered", result);
+      res.status(201).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Contact failed to register." });
+    });
+});
+
+router.put("/update", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .updateContact(info)
+    .then(result => {
+      console.log("contact updated", result);
+      res.status(200).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Contact failed to update." });
+    });
+});
+
+router.delete("/delete", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .deleteContact(info)
+    .then(result => {
+      console.log("contact deleted", result);
+      res.status(200).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Contact failed to delete." });
+    });
+});
+
+router.post("/social/register", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .registerSocial(info)
+    .then(result => {
+      console.log("social registered", result);
+      res.status(201).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Social info failed to register." });
+    });
+});
+
+router.put("/social/update", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .updateSocial(info)
+    .then(result => {
+      console.log("social updated", result);
+      res.status(200).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Social info failed to update." });
+    });
+});
+
+router.delete("/social/delete", protected, (req, res) => {
+  const info = req.body;
+
+  contactDB
+    .deleteSocial(info)
+    .then(result => {
+      console.log("social deleted", result);
+      res.status(200).json({ result });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Social info failed to delete." });
+    });
+});
+
+module.exports = router;
